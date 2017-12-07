daab4b407912686bcdd7a68c01ae08c7ba605ec9
fankay <fankai@kaishengit.com>
2017-12-07 15:57:08 +0800

    thymeleaf模板引擎的使用

--- springboot/src/main/java/com/kaishengit/controller/HomeController.java
+++ springboot/src/main/java/com/kaishengit/controller/HomeController.java
@@ -1,13 +1,29 @@
 package com.kaishengit.controller;
 
+import com.kaishengit.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
 public class HomeController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello,SpringBoot";
+    public String hello(Model model, HttpSession session) {
+
+        model.addAttribute("message","SpringBoot Demo 1.0");
+        model.addAttribute("user",new User(1009,"lisi","北京"));
+        session.setAttribute("msg","Session Message");
+
+        List<String> userNameList = Arrays.asList("aa","bb","cc","dd");
+        model.addAttribute("userNameList",userNameList);
+
+        return "index";
     }
 }
