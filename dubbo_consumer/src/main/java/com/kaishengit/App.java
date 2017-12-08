3798836929562a380f62a970034cd7d30523af77
fankay <fankai@kaishengit.com>
2017-12-08 16:19:06 +0800

    alibaba dubbo quickstart

---
+++ dubbo_consumer/src/main/java/com/kaishengit/App.java
@@ -0,0 +1,23 @@
+package com.kaishengit;
+
+import com.kaishengit.service.ProductServcie;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-dubbo-consumer.xml");
+        ProductServcie productServcie = (ProductServcie) context.getBean("rpcProductService");
+        List<String> productNames = productServcie.findAllProductNames();
+        for(String name : productNames) {
+            System.out.println(name);
+        }
+        productServcie.save("hello,Dubbo");
+        System.in.read();
+
+    }
+}
