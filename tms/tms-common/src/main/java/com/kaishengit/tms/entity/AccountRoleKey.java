448d84a7da886a1f2a3461163c9e4631fff63409
fankay <fankai@kaishengit.com>
2017-12-13 14:44:32 +0800

    TMS结合dubbo服务

---
+++ tms/tms-common/src/main/java/com/kaishengit/tms/entity/AccountRoleKey.java
@@ -0,0 +1,30 @@
+package com.kaishengit.tms.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author 
+ */
+public class AccountRoleKey implements Serializable {
+    private Integer roleId;
+
+    private Integer accountId;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Integer accountId) {
+        this.accountId = accountId;
+    }
+}
