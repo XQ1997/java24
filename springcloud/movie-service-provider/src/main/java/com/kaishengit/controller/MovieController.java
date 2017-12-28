844674c59d5ff2690bdcae5a1b944b26a9ab62b3
fankay <fankai@kaishengit.com>
2017-12-28 15:48:48 +0800

    Spring cloud config

--- springcloud/movie-service-provider/src/main/java/com/kaishengit/controller/MovieController.java
+++ springcloud/movie-service-provider/src/main/java/com/kaishengit/controller/MovieController.java
@@ -3,15 +3,26 @@ package com.kaishengit.controller;
 import com.kaishengit.pojo.Movie;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class MovieController {
 
     private Logger logger = LoggerFactory.getLogger(MovieController.class);
+
+    @Value("${qiniu.key}")
+    private String qiniuKey;
+
+    @GetMapping("/qn")
+    public String qiniu() {
+        return qiniuKey;
+    }
 
     @GetMapping("/movie/{id:\\d+}")
     public Movie findMovie(@PathVariable Integer id) {
